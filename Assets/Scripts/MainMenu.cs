e32f3de338d842530fa0eaf90f76b66915c35fce
William O'Brien <williamobrien94@gmail.com>
2018-02-26 21:52:19 +0000

    Changes: Basic Main Menu added with play button that changes scene and text that gets an integer from PlayerPrefs to display the high score set in another scene.

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour {
+    public string levelName;
+    public Text scoreText;
+    int score;
+
+	// Use this for initialization
+	void Start () {
+        //PlayerPrefs.SetInt("score", 10);//Just here for testing
+        score = PlayerPrefs.GetInt("score", 0);
+        scoreText.text = "High Score: " + score;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void play()
+    {
+        SceneManager.LoadScene(levelName);
+    }
+}
