963bc3593671efe70bd04f65381e9f437e894087
William O'Brien <williamobrien94@gmail.com>
2018-02-28 10:33:31 +0000

    Changes: -In game UI partially implemented (HUD & Pause Menu) -Enabling and disabling canvases still left to do

---
+++ Assets/Scripts/GameUI.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameUI : MonoBehaviour {
+
+    public Text scoreText;
+    public Text pauseScore;
+    public int score = 0;
+    int highScore;
+    // Use this for initialization
+    void Start () {
+		highScore = PlayerPrefs.GetInt("score", 0);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        scoreText.text = "Score: " + score;
+        pauseScore.text = "Current Score: " + score;
+        if (score > highScore)
+            PlayerPrefs.SetInt("score", score);
+	}
+}
